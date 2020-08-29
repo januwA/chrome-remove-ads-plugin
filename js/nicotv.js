40109da76eebef2a0d94cb4f1a7b75b12f933220
januwA <ajanuw1995@gmail.com>
2020-08-29 22:52:45 +0800

    update nicotv.club

--- js/nicotv.js
+++ js/nicotv.js
@@ -1,1 +1,5 @@
-removeDom('.container.ff-bg')
+document.querySelector(".container.ff-bg")?.remove();
+
+setInterval(function () {
+  document.querySelector("#ff-totop")?.nextSibling?.remove()
+}, 1000);
