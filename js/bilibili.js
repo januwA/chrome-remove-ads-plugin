e9524d1af6f786b7ac8a2977b3d1bae021133bcf
januwA <ajanuw1995@gmail.com>
2020-08-31 02:14:04 +0800

    屏蔽直播间

--- js/bilibili.js
+++ js/bilibili.js
@@ -1,5 +1,45 @@
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (!msg.flag) return;
+
+  if (msg.flag & MsgFlags.bilibili_com.load_liveid) {
+    if (msg?.data?.ids?.length) {
+      Array.from(document.querySelector(".list.clearfix").children).forEach(
+        (li) => {
+          const m = li.querySelector("a")?.href.match(/\/([0-9]+)\?/);
+          if (m && m[1] && msg.data?.ids?.includes(m[1])) {
+            li.style.transform = "scale(0)";
+          }
+        }
+      );
+    }
+  }
+});
+
 setInterval(() => {
   removeDom(".banner-card");
 }, 400);
 
-removeDom('#reportFirst2')
+removeDom("#reportFirst2");
+
+document.addEventListener("contextmenu", (e) => {
+  const a = e.path.find((it) => {
+    return it?.classList?.contains("dp-block");
+  });
+  if (a && a.nodeName === "A".toUpperCase()) {
+    const m = a.href.match(/\/([0-9]+)\?/);
+    if (!m || !m[1]) {
+      console.warn("not match id");
+      return;
+    }
+
+    const id = m[1].trim();
+    // 将消息发送到bk
+    chrome.runtime.sendMessage(
+      new MsgData(MsgFlags.bilibili_com.contextmenu_liveid, {
+        id,
+      })
+    );
+  }
+});
+
+chrome.runtime.sendMessage(new MsgData(MsgFlags.bilibili_com.load_liveid));
--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,20 @@ function removeDom(select) {
     it.remove();
   });
 }
+
+class MsgData {
+  constructor(flag, data) {
+    this.flag = flag;
+    this.data = data;
+  }
+}
+
+const MsgFlags = {
+  bilibili_com: {
+    // 屏蔽直播间消息
+    contextmenu_liveid: 1 << 1,
+
+    // 获取屏蔽直播间 id列表
+    load_liveid: 1 << 2,
+  },
+};
