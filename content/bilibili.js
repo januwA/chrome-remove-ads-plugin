80f4ebed68bef0e854b5e7e07c87d44d246a6f06
januwA <ajanuw1995@gmail.com>
2021-06-20 17:07:25 +0800

    update desktophut

--- content/bilibili.js
+++ content/bilibili.js
@@ -1,32 +1,3 @@
-// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-//   if (!msg.flag) return;
-
-//   if (msg.flag & MsgFlags.bilibili_com.load_liveid) {
-//     if (!msg?.data?.ids?.length) return;
-
-//     const listLi = Array.from(
-//       document.querySelector("ul.list")?.children ?? []
-//     );
-//     if (!listLi.length) return;
-
-//     let index = 0;
-//     listLi.forEach((li) => {
-//       const m = li.querySelector("a")?.href.match(/\/([0-9]+)\?/);
-//       const liveID = m[1] ?? 0;
-//       if (msg?.data?.isAdd) {
-//         if (m && liveID && msg.data?.ids?.includes(liveID)) {
-//           li.style.filter = "blur(20px)";
-//           if (index > msg.data?.ids.length) return true;
-//           index++;
-//         }
-//       } else {
-//         if (m && liveID && liveID == msg?.data?.id)
-//           return (li.style.filter = "blur(0px)");
-//       }
-//     });
-//   }
-// });
-
 setInterval(() => {
   removeDom(".banner-card");
   removeDom("#reportFirst2");
@@ -47,26 +18,3 @@ setInterval(() => {
   removeDom("#bili_guochuang");
   removeDom(".live-tabs");
 }, 1000);
-
-// document.addEventListener("contextmenu", (e) => {
-//   const a = e.path.find((it) => {
-//     return it?.classList?.contains("dp-block");
-//   });
-//   if (a && a.nodeName === "A".toUpperCase()) {
-//     const m = a.href.match(/\/([0-9]+)\?/);
-//     if (!m || !m[1]) {
-//       console.warn("not match id");
-//       return;
-//     }
-
-//     const id = m[1].trim();
-//     // 将消息发送到bk
-//     chrome.runtime.sendMessage(
-//       new MsgData(MsgFlags.bilibili_com.contextmenu_liveid, {
-//         id,
-//       })
-//     );
-//   }
-// });
-
-// chrome.runtime.sendMessage(new MsgData(MsgFlags.bilibili_com.load_liveid));
--- content/desktophut.js
+++ content/desktophut.js
@@ -1,22 +1,19 @@
-removeDom(".g1-injected-unit");
-removeDom("#custom_html-7");
-removeDom(".g1-sticky-widget-wrapper");
-removeDom("#google_esf");
-
 setInterval(() => {
-  document.querySelectorAll("iframe")?.forEach((it) => {
-    if (it.hasAttribute("sandbox")) {
-      it.remove();
+  let lastElementChild = document.body.lastElementChild;
+
+  // 删除强制的弹窗
+  if (
+    lastElementChild.className.endsWith("-wrapper") ||
+    lastElementChild.className.endsWith(" active")
+  ) {
+    lastElementChild.remove();
+  }
+
+  // 删除模糊效果
+  for (const it of document.body.classList) {
+    if (it.endsWith("-blur")) {
+      document.body.classList.remove(it);
+      break;
     }
-  });
-
-  document.querySelectorAll("div").forEach((it) => {
-    if (it.style.zIndex && parseInt(it.style.zIndex) > 5000) {
-      it.remove();
-    }
-  });
-
-  document.querySelector('#vid-container0')?.remove()
-  document.querySelector('.ads')?.remove()
-
+  }
 }, 500);
