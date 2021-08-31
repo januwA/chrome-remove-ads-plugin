fa2df4cd88c551e00dc3731cbadefcf9e0637a76
januwA <ajanuw1995@gmail.com>
2021-08-31 08:44:56 +0800

    add gitee.com

---
+++ content/gitee.com.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+  document.querySelector(".recommend-container")?.remove();
+  document.querySelector("#git-footer-main")?.remove();
+  document.querySelector(".wechat-banner")?.remove();
+}, 500);
