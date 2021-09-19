407c403afe7c3f86266463fc1e68bfe9456d200d
januwA <ajanuw1995@gmail.com>
2021-09-19 22:10:28 +0800

    update bilibili

--- content/bilibili.js
+++ content/bilibili.js
@@ -1,3 +1,8 @@
+// namespace
+const _bili = {
+  a: true,
+};
+
 setInterval(() => {
   removeDom(".banner-card");
   removeDom("#reportFirst2");
@@ -17,4 +22,20 @@ setInterval(() => {
   removeDom("#bili_technology");
   removeDom("#bili_guochuang");
   removeDom(".live-tabs");
+
+  // 屏蔽首页推荐的垃圾信息
+  if (_bili.a) {
+    const removeIt = document.querySelector("#internationalHeader")?.nextSibling
+      ?.firstChild;
+
+    if (removeIt?.classList?.contains?.("space-between") ?? false) {
+      removeIt?.remove();
+    }
+
+    // 屏蔽banner上的背景图
+    const baner = document.querySelector(".bili-banner");
+    if (baner) baner.style.backgroundImage = "none";
+
+    _bili.a = false;
+  }
 }, 1000);
